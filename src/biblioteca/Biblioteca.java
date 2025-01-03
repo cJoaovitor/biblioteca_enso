9ec2d03ab575e180de2e74e3b1e4367b032ace03
joao <jvs41@aluno.ifal.edu.br>
2025-01-03 15:01:14 -0300

    arraylis adicionadas

--- src/biblioteca/Biblioteca.java
+++ src/biblioteca/Biblioteca.java
@@ -1,11 +1,17 @@
 
 package biblioteca;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Biblioteca {
 
   
     public static void main(String[] args) {
+        ArrayList<Usuario> us = new ArrayList<Usuario>();
+        ArrayList<Bibliotecario> bli  = new ArrayList<Bibliotecario>();
+        ArrayList<Administrardor> adm = new ArrayList<Administrardor>();
+        ArrayList<Livro> livros = new ArrayList<Livro>();
+        
     }
     
 }
